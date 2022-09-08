f7e6cb35661e6e7798c2497de9b97f136d149d79
Dawn242 <dawnmanifesttob88@gmail.com>
2022-09-08 16:15:05 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the string with the printf fuction
+ *
+ * Return: Always 0 (this is successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
